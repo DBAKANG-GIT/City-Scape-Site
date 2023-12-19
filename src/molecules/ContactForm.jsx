6cf7514d271ca78ebae907eb6292434deae56880
David Akang <akangdavd68@yahoo.com>
2023-12-19 08:57:29 +0000

    contact

--- src/molecules/ContactForm.jsx
+++ src/molecules/ContactForm.jsx
@@ -1,45 +1,25 @@
 import Button from 'src/atoms/buttons/button';
-import InputField from 'src/atoms/inputField/input-field';
-import Recaptcha from 'src/atoms/recaptcha/recaptcha-netlify';
+// import InputField from 'src/atoms/inputField/input-field';
 
-const ContactForm = (enableRecaptcha) => {
+const ContactForm = () => {
   return (
-    <form
-      className="w-full py-5"
-      action="/contact"
-      method="post"
-      name="callback-form"
-    >
+    <form className="w-full py-5" action="/" method="post" name="callback-form">
       <input type="hidden" name="form-name" value="callback-form" />
-      <InputField type="text" id="name" name="name" placeholder="Name" />
 
-      <InputField
-        type="email"
-        id="email"
-        name="email"
-        placeholder="Email Address"
-      />
-      <InputField
-        type="text"
-        id="subject"
-        name="subject"
-        placeholder="Subject"
-      />
-      <InputField
-        type="tel"
-        id="phone"
-        name="phone"
-        placeholder="Phone Number"
-      />
+      <input type="text" name="name" placeholder="Name" />
+
+      <input type="email" name="email" placeholder="Email Address" />
+
+      <input type="text" name="subject" placeholder="Subject" />
+
+      <input type="tel" name="phone" placeholder="Phone Number" />
 
       <textarea
         className="w-full h-36 border border-black py-2 px-4 mb-4"
         type="text"
-        id="message"
         name="message"
         placeholder="Enter Message"
       ></textarea>
-      {enableRecaptcha ? <Recaptcha /> : null}
       <Button />
     </form>
   );
