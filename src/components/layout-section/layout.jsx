9327abe8e83c1308946425c79aab1cee0a48bf84
David Akang <akangdavd68@yahoo.com>
2023-12-29 01:11:11 +0000

    Update page titles in index.html and app.jsx

--- src/components/layout-section/layout.jsx
+++ src/components/layout-section/layout.jsx
@@ -1,8 +1,11 @@
 import Footer from '../footer-section/footer';
 import Navbar from '../navbar-section/navbar';
 import PropTypes from 'prop-types';
+import useDocumentTitle from '../../hooks/useDocumentTitle';
 
-const Layout = ({ children }) => {
+const Layout = ({ children, title }) => {
+  useDocumentTitle(title);
+
   return (
     <div>
       <Navbar
@@ -22,6 +25,7 @@ const Layout = ({ children }) => {
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
+  title: PropTypes.string.isRequired,
 };
 
 export default Layout;
